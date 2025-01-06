533f00a88185a807b0de76f601e3632a4b79c4fa
Richese Lync <richesel@oa-k2.msk.21-school.ru>
2025-01-06 20:34:22 +0300

    fix err 500

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,33 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 from django.http import Http404
 
 # Create your views here.
+
+
+def post_list(reqest):
+    post_list = Post.published.all()
+    # Построение разбивки с 3 постами на странице
+    paginator = Paginator(post_list, 3)
+    page_number = reqest.GET.get('page', 1)
+    
+    try:
+        posts = paginator.page(page_number)
+
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        reqest,
+        'blog/post/list.html',
+        {'posts' : posts}
+    )
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -19,17 +43,18 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post' : post}
     )
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html' 
+
 # def post_detail(request, id):
 #     post = get_object_or_404(Post, id=id)
 #     return render(request, 'blog/post_detail.html',{'post' : post})
     
-def post_list(reqest):
-    posts = Post.published.all()
-    return render(
-        reqest,
-        'blog/post/list.html',
-        {'posts' : posts}
-    )
+
 
 # def post_detail(request, id):
 #     try:
